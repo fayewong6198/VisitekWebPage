6dad6dfe16de2605c9cec985e4bda74de25284cc
fayewong6198 <trandaosimanh@gmail.com>
2020-05-04 11:39:41 +0700

    cc

--- VisitekWebPage/settings.py
+++ VisitekWebPage/settings.py
@@ -143,3 +143,16 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
+
+
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+except Exception as e:
+    pass
